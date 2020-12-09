0c2f8e9000a5eafdef1596efa0e27044da0c6fd5
Miss-chan <952838607@qq.com>
2020-12-09 10:43:05 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="util_test",
+    version="0.1.0",
+    keywords=("pip", "util", "time", " tool"),
+    description="util tool",
+    long_description="util tool",
+    license="MIT Licence",
+    url="https://github.com/fengmm521/pipProject",
+    author="mage",
+    author_email="mage@woodcol.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
